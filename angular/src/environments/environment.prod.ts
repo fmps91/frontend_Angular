9dba12e604e2f6f36c818737aba8075cdf02c6a6
fmps91 <f@MacBook-Pro-de-f.local>
2020-07-28 10:12:15 -0500

    config produc 2

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  base: 'http://localhost:8090/api/v1/',
+  //base: 'http://192.168.99.100:8090/api/v1/',
+  port: '',
+  ancho: 0,
+  alto: 0,
+  session: false,
+  init: true,
+  superuser: "admin@sgd.com",
+  superpassword: "admin",
 };
